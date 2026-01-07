613590206ca5f14a0447f7fb3193b766f319c642
Hosam Zewain <hosam.zewain@gmail.com>
2026-01-07 13:19:53 +0200

    fix: use relative api path

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Tour, BookingDetails } from '../types';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = '/api';
 
 export const api = axios.create({
     baseURL: API_URL,
